6647975130df942e0a58b1a4380f1311b474a367
mjavadHasanzade <hjavad522@gmail.com>
2024-03-04 18:37:52 +0330

    feat: add full drft component and it's data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,24 @@ import { api } from "~/trpc/server";
 
 const MEMOS = [
   {
-      title: "Salsile project brief",
-      description: "No, going all perfect! Let me show you images of the project",
-      cover: "/images/1.jpg",
-      score: 5,
-      mode: "😍"
+    title: "Salsile project brief",
+    description: "No, going all perfect! Let me show you images of the project",
+    content: "Salsile Inc.is a well- established fashion retailer specializing in high - quality clothing and accessories for men and women.The client is looking to revamp their existing e - commerce website to enhance user experience, improve overall aesthetics, and increase online sales.The new design should reflect their brand identity as a modern, and customer - centric fashion store.",
+    cover: "/images/1.jpg",
+    score: 5,
+    mode: "😍"
   }, {
-      title: "A Good Day at new york",
-      description: "No, going all perfect! Let me show you images of the project",
-      cover: "/images/2.jpg",
-      score: 3,
-      mode: "😊"
+    title: "A Good Day at new york",
+    description: "No, going all perfect! Let me show you images of the project",
+    cover: "/images/2.jpg",
+    score: 3,
+    mode: "😊"
   }, {
-      title: "Sad moments",
-      description: "No, going all perfect! Let me show you images of the project",
-      cover: "/images/3.jpg",
-      score: 2,
-      mode: "😐"
+    title: "Sad moments",
+    description: "No, going all perfect! Let me show you images of the project",
+    cover: "/images/3.jpg",
+    score: 2,
+    mode: "😐"
   }
 ]
 
@@ -36,28 +37,12 @@ export default async function Home() {
     <div className="min-h-screen">
       <div className="flex">
         <div className="w-1/2 pr-4">
-          <AllMemories />
+          <AllMemories memories={MEMOS} />
         </div>
         <div className="w-1/2">
-          <Draft />
+          <Draft title={MEMOS[0]!.title} cover={MEMOS[0]!.cover} content={MEMOS[0]!.content} mode={MEMOS[0]!.mode} />
         </div>
       </div>
-    </div>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.memory.getLatest.query();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.title}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      {/* <CreatePost /> */}
     </div>
   );
 }
--- src/components/organisms/draft.tsx
+++ src/components/organisms/draft.tsx
@@ -1,17 +1,45 @@
+import Link from 'next/link';
 import React from 'react'
 
 type Props = {
   title: string;
   cover: string;
-  description: string;
+  content: string;
+  mode: string;
 }
 
 const Draft = (props: Props) => {
   return (
     <section>
       <div className="p-4 bg-white rounded-xl">
-        <div className="flex">
-          <img src={draftMemory.cover} alt="" className='w-6 h-8' />
+        <div className="flex items-center">
+          <img src={props.cover} alt="" className='w-10 h-8 rounded-lg object-cover' />
+          <h2 className='text-lg ml-4 font-bold mb-2 truncate '>{props.title}</h2>
+
+          <Link href={"/"} className="px-3 py-1 bg-[#8CA6FC] text-white rounded-lg text-sm ml-auto">
+            Edit
+          </Link>
+        </div>
+        <div className="h-[2px] w-full bg-gray-300 my-4"></div>
+        <div className="">
+          <div className="mb-4">
+            <span className='text-xs text-[#8CA6FC] bg-[#E8E7F9] rounded-xl font-bold p-1 px-2'>
+              mode: {props.mode}
+            </span>
+          </div>
+
+          <p className='text-sm my-4 text-typo leading-7'>
+            {props.content}
+          </p>
+
+          <div className="flex justify-end mt-4">
+            <Link href={"/"} className="px-3 py-1 mr-4 text-[#8CA6FC] rounded-lg text-sm">
+              Save
+            </Link>
+            <Link href={"/"} className="px-3 py-1 bg-[#8CA6FC] text-white rounded-lg text-sm">
+              Publish
+            </Link>
+          </div>
         </div>
       </div>
     </section>
