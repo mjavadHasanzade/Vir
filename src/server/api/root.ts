a8b9a99e4cc867252f3b584ea7dc777fb98369a3
mjavadHasanzade <hjavad522@gmail.com>
2024-03-04 17:47:07 +0330

    refactor: remove inital endpoints

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { memoryRouter } from "~/server/api/routers/memory";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  memory: memoryRouter,
 });
 
 // export type definition of API
