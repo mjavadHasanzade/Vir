ef353ec7311ad9708ae7cd6a84c824490b009bf2
mjavadHasanzade <hjavad522@gmail.com>
2024-03-05 16:57:46 +0330

    feat: add seed na dlast draft endpoints

--- src/server/api/routers/memory.ts
+++ src/server/api/routers/memory.ts
@@ -1,8 +1,21 @@
+import { memoriesMock } from "mock";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const memoryRouter = createTRPCRouter({
+
+  seed: publicProcedure.query(({ ctx }) => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    memoriesMock.map(async (data) =>
+      await ctx.db.memory.create({
+        data
+      })
+    )
+    return;
+
+  }),
+
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -24,9 +37,18 @@ export const memoryRouter = createTRPCRouter({
       });
     }),
 
+  getLastDraft: publicProcedure.query(async ({ ctx }) => {
+    return ctx.db.memory.findFirst({
+      where: {
+        published: false
+      }
+    })
+  }),
+
   getLatest: publicProcedure.query(async ({ ctx }) => {
     const memories = await ctx.db.memory.findMany({
       orderBy: { createdAt: "desc" },
+      where: { published: true }
     });
 
     return {
