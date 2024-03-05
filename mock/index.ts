9f712abf0b5b65e69a4ec36fccc9b5729c9672c5
mjavadHasanzade <hjavad522@gmail.com>
2024-03-05 16:55:07 +0330

    feat: add mock data to seed

---
+++ mock/index.ts
@@ -0,0 +1,42 @@
+export const memoriesMock = [
+    {
+        "title": "Saying Goodbye to a Beloved Pet",
+        "description": "The day started with a heaviness in my heart as I made the painful decision to bid farewell to my furry friend.",
+        "content": "Losing a beloved pet is one of the most heartbreaking experiences. It was a somber day when I had to say goodbye to my loyal and cherished companion. My pet had been a source of unconditional love and joy in my life, and their absence left a void that was difficult to fill.",
+        "cover": "/images/4.jpg",
+        "score": 2,
+        "mode": "😣",
+        "createdAt": "2024-03-04T18:44:13.541Z",
+        "updatedAt": "2024-03-04T18:44:39.421Z"
+    },
+    {
+        "title": "Serene Sunset at the Beach",
+        "description": "As I stood barefoot in the soft sand, the gentle ocean breeze caressed my face, carrying with it the scent of saltwater.",
+        "content": "It was a warm summer evening when I found myself on a tranquil beach, ready to witness a breathtaking sunset. The sky was painted with hues of orange, pink, and purple, creating a mesmerizing backdrop for the scene that was about to unfold.",
+        "cover": "/images/2416.jpg",
+        "score": 5,
+        "mode": "❤️",
+        "createdAt": "2024-03-04T18:40:58.681Z",
+        "updatedAt": "2024-03-04T18:44:29.355Z"
+    },
+    {
+        "title": "A Good Day at New York",
+        "description": "I strolled along the winding pathways, taking in the picturesque views of the park's iconic landmarks.",
+        "content": "On a beautiful sunny day in New York City, I decided to explore Central Park. The park was bustling with activity and filled with vibrant energy. As I entered the park, I was greeted by the sound of laughter, the scent of blooming flowers, and the sight of families enjoying picnics on the lush green lawns.",
+        "cover": "/images/2.jpg",
+        "score": 3,
+        "mode": "😊",
+        "createdAt": "2024-03-03T11:47:16.778Z",
+        "updatedAt": "2024-03-04T18:20:57.554Z"
+    },
+    {
+        "title": "Salsile project brief",
+        "description": "No, going all perfect! Let me show you images of the project",
+        "content": "Salsile Inc. is a well-established fashion retailer specializing in high-quality clothing and accessories for men and women. The client is looking to revamp their existing e-commerce website to enhance user experience, improve overall aesthetics, and increase online sales. The new design should reflect their brand identity as a modern and customer-centric fashion store.",
+        "cover": "/images/1.jpg",
+        "score": 4,
+        "mode": "😍",
+        "createdAt": "2024-03-03T11:11:10.457Z",
+        "updatedAt": "2024-03-04T18:17:16.514Z"
+    }
+]
